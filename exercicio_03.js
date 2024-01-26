e9a0cad59830a41373bd83587c8e68c5bdf02e6e
Gabriel Sferra <618608@univem.edu.br>
2024-01-26 09:28:25 -0300

    update: Adicionado testes exercicio 03

--- exercicio_03.js
+++ exercicio_03.js
@@ -7,10 +7,18 @@ for (let i = 1; i <= 100; i++) {
 }
 
 
+// Split
+console.log("-=-=-=-=-=-=-=-=-")
+
+
 /**
  * Funções: Escreva uma função que aceite dois números como argumentos e retorne sua multiplicação.
 */
 
-function multiply(a, b) {
+function multiplicaco(a, b) {
   return a * b
 }
+
+console.log(`10 x 25: ${multiplicaco(10, 25)}`)
+console.log(`20 x 25: ${multiplicaco(20, 25)}`)
+console.log(`30 x 25: ${multiplicaco(30, 25)}`)
